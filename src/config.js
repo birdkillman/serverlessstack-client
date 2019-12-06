49987b978dd105a86885db83eb8766f6299e4c77
Hill Yang <birdkillman@gmail.com>
2019-12-06 17:43:24 -0500

    Setting up our React app

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+        REGION: "us-east-1",
+        BUCKET: "notes-app-uploads-test-gang"
+    },
+    apiGateway: {
+        REGION: "us-east-1",
+        URL: "https://sh19zu7uuk.execute-api.us-east-1.amazonaws.com/prod"
+    },
+    cognito: {
+        REGION: "us-east-1",
+        USER_POOL_ID: "us-east-1_XTSulcRhI",
+        APP_CLIENT_ID: "78see8mvo9s0divqj59eeg3mk",
+        IDENTITY_POOL_ID: "us-east-1:979399aa-d8f8-42ed-8ac9-f50a70e2da65"
+    }
+};
